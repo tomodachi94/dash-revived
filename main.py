832ef6bcd263aefb28f0687fecd35bbdcb8331cc
Tomodachi94 <tomodachi94@protonmail.com>
2021-04-04 09:12:17 -0700

    Format code, pyproject.toml

--- main.py
+++ main.py
@@ -3,26 +3,25 @@ from keep_alive import keep_alive
 from discord.ext import commands
 
 bot = commands.Bot(
-	command_prefix="_",  # Change to desired prefix
-	case_insensitive=True  # Commands aren't case-sensitive
+    command_prefix="_",  # Change to desired prefix
+    case_insensitive=True,  # Commands aren't case-sensitive
 )
 
 bot.author_id = 769063453637476403  # Change to your discord id!!!
 
-@bot.event 
+
+@bot.event
 async def on_ready():  # When the bot is ready
     print("I'm in")
     print(bot.user)  # Prints the bot's username and identifier
 
 
-extensions = [
-	'cogs.cog_example'  # Same name as it would be if you were importing it
-]
+extensions = ["cogs.dev"]  # Same name as it would be if you were importing it
 
-if __name__ == '__main__':  # Ensures this is the file being ran
-	for extension in extensions:
-		bot.load_extension(extension)  # Loades every extension.
+if __name__ == "__main__":  # Ensures this is the file being ran
+    for extension in extensions:
+        bot.load_extension(extension)  # Loades every extension.
 
 keep_alive()  # Starts a webserver to be pinged.
-token = os.environ.get("DISCORD_BOT_SECRET") 
+token = os.environ.get("DISCORD_BOT_SECRET")
 bot.run(token)  # Starts the bot
